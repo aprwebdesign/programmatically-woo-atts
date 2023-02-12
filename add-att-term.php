26805db00facc17b3dff2a0cf4b7057afd5e877e
aprwebdesign <Info@aprwebdesign.com>
2023-02-12 15:02:49 +0100

    Update add-att-term.php

--- add-att-term.php
+++ add-att-term.php
@@ -1,21 +1,21 @@
 // Add the attributes although i dont know why the value issn't added here..  
  update_post_meta($product_id, '_product_attributes', array(
 'pa_option1' => array(
-'name' => 'pa_option1',
+'name' => 'pa_name',
 'value' => $pa_option1,
 'position' => 1,
 'is_visible' => 1,
 'is_taxonomy' => 1
 ),
 'pa_option2' => array(
-'name' => 'pa_option2',
+'name' => 'pa_name2',
 'value' => $pa_option2,
 'position' => 1,
 'is_visible' => 1,
 'is_taxonomy' => 1
 ),
 'pa_option3' => array(
-'name' => 'pa_option3',
+'name' => 'pa_name2',
 'value' => $pa_option3,
 'position' => 1,
 'is_visible' => 1,
@@ -26,7 +26,7 @@
 // loop through the attributes and add the right term
 
 $product_id = $product_id;
-$attribute_names = array('pa_option2', 'pa_option1', 'pa_option3');
+$attribute_names = array('pa_name', 'pa_name2', 'pa_name3');
 $term_ids = array($pa_option2, $pa_option1, $pa_option3);
 
 for ($i=0; $i < count($attribute_names); $i++) { 
